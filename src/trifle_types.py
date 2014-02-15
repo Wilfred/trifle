d02fa05a032ea03273f16c8e5de03c6663454fbe
Wilfred Hughes <me@wilfred.me.uk>
2014-02-15 15:38:52 +0000

    Removing completed TODO.

--- src/trifle_types.py
+++ src/trifle_types.py
@@ -156,7 +156,6 @@ class List(TrifleType):
 
 
 # TODO: reader macro for these
-# TODO: a way of creating these without opening a file
 # TODO: unit test equal? for these
 # TODO: make these mutable
 # TODO: a better name for these
