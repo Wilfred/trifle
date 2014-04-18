d32c017b45a8660f5f43230aa81c81a0db7f2e8b
Wilfred Hughes <me@wilfred.me.uk>
2014-04-18 11:03:18 +0100

    If given the wrong number of arguments, state how many given.

--- src/arguments.py
+++ src/arguments.py
@@ -8,8 +8,6 @@ ANY = -1
 
 
 # TODO: ideally we'd name the arguments we're expected
-# TODO: we should probably show both the total number of args and the
-# args given.
 def check_args(name, args, min=ANY, max=ANY):
     """Check whether args is the right length for this function or special
     expression.
@@ -21,26 +19,26 @@ def check_args(name, args, min=ANY, max=ANY):
         if len(args) != min:
             if min == 1:
                 raise ArityError(
-                    u"%s takes 1 argument, but got: %s" % (name, List(args).repr())
+                    u"%s takes 1 argument, but got %d: %s" % (name, len(args), List(args).repr())
                 )
             else:
                 raise ArityError(
-                    u"%s takes %d arguments, but got: %s" % (name, min, List(args).repr())
+                    u"%s takes %d arguments, but got %d: %s" % (name, len(args), min, List(args).repr())
                 )
 
     elif max == ANY:
         if len(args) < min:
             if min == 1:
                 raise ArityError(
-                    u"%s takes at least 1 argument, but got: %s" % (name, List(args).repr())
+                    u"%s takes at least 1 argument, but got %d: %s" % (name, len(args), List(args).repr())
                 )
             else:
                 raise ArityError(
-                    u"%s takes at least %d arguments, but got: %s" % (name, min, List(args).repr())
+                    u"%s takes at least %d arguments, but got %d: %s" % (name, min, len(args), List(args).repr())
                 )
 
     else:
         if not (min <= len(args) <= max):
             raise ArityError(
-                u"%s takes between %d and %d arguments, but got: %s" % (name, min, max, List(args).repr())
+                u"%s takes between %d and %d arguments, but got %d: %s" % (name, min, max, len(args), List(args).repr())
             )
